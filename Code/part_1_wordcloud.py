8d175eb2d1a175d1a949560ff6e99a5de568b7cc
Rosa Zhu <rooosa.juju@gmail.com>
2022-04-11 17:18:56 -0500

    Update part_1_wordcloud.py

--- Code/part_1_wordcloud.py
+++ Code/part_1_wordcloud.py
@@ -1,4 +1,4 @@
-# first edition
+# FIRST EDITION
 #!/usr/bin/env python
 import sys
 
@@ -8,13 +8,11 @@ for line in sys.stdin:
   year = words[0]
   dis = words[1]
   des = words[2]
-  
   if year == "2020":
     des = des.replace(" ", "~")
-    
-  print ('%s' %des)
+    print ('%s' %des)
 
-# second edition
+# SECOND EDITION
 #!/usr/bin/env python
 import sys
 
@@ -26,4 +24,32 @@ for line in sys.stdin:
   des = words[2]
   if year == "2015":
     des = des.replace(" ", "~")
-  print ('%s' %des)
+    print ('%s' %des)
+
+# THIRD EDITION
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+  line = line.strip()
+  words = line.split("\t")
+  year = words[0]
+  dis = words[1]
+  des = words[2]
+  if year == "2010":
+     des = des.replace(" ", "~")
+     print ('%s' %des)
+    
+# FOURTH EDITION
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+  line = line.strip()
+  words = line.split("\t")
+  year = words[0]
+  dis = words[1]
+  des = words[2]
+  if year == "2005":
+     des = des.replace(" ", "~")
+     print ('%s' %des)
